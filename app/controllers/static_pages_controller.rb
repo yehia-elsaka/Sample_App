3aa0f4575fcc867129a06743639f624e12958ede
yehia-elsaka <yahya.elsaka@gmail.com>
2016-10-31 14:10:49 +0200

    Chapter 5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,12 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def api_sample
+    uid = params[:user_id]
+    render :json => {:uid=> uid, :msg => "API HELLO WORLD"}
+  end
+
+  def contact
+  end
 end
